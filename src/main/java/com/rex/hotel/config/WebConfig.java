3b1ba3c84738515dc2c334a121df685a276114b2
DanhThai11 <1050080033@sv.hcmunre.edu.vn>
2025-06-06 01:53:18 +0700

    Sua server lần thứ 7 TvT

--- src/main/java/com/rex/hotel/config/WebConfig.java
+++ src/main/java/com/rex/hotel/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://v0-rex-hotel.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://hotelclient-2075e.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
