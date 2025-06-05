610c36cf49440988c0f570a1b2a37b1304e80edf
DanhThai11 <1050080033@sv.hcmunre.edu.vn>
2025-06-05 22:29:58 +0700

    Add 22:29

--- src/main/java/com/rex/hotel/RexHotelApplication.java
+++ src/main/java/com/rex/hotel/RexHotelApplication.java
@@ -1,5 +1,6 @@
 package com.rex.hotel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,7 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class RexHotelApplication {
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(RexHotelApplication.class, args);
     }
 }
--- target/generated-sources/annotations/com/rex/hotel/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/rex/hotel/mapper/UserMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-05T21:21:00+0700",
+    date = "2025-06-05T21:39:44+0700",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 21.0.2 (Eclipse Adoptium)"
 )
 @Component
